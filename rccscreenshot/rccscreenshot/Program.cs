581ef945efde9eea737dd4b1edad86aa0edc61b8
VPS <yakov5776@gmail.com>
2022-03-11 15:49:28 -0500

    Add Screenshot Helper SRC

---
+++ rccscreenshot/rccscreenshot/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rccscreenshot
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+        [DllImport("user32.dll")]
+        public static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, int nFlags);
+
+        static void Main(string[] args)
+        {
+            Process[] proc = Process.GetProcessesByName("RCCServiceSoap");
+            if (proc.Length == 0) return;
+
+            Bitmap image = PrintWindow(proc[0].MainWindowHandle);
+            image.Save("screenshot.png");
+        }
+
+        public static Bitmap PrintWindow(IntPtr hwnd)
+        {
+            RECT rc;
+            GetWindowRect(hwnd, out rc);
+
+            Bitmap bmp = new Bitmap(rc.Width, rc.Height, PixelFormat.Format32bppArgb);
+            Graphics gfxBmp = Graphics.FromImage(bmp);
+            IntPtr hdcBitmap = gfxBmp.GetHdc();
+
+            PrintWindow(hwnd, hdcBitmap, 0);
+
+            gfxBmp.ReleaseHdc(hdcBitmap);
+            gfxBmp.Dispose();
+
+            return bmp;
+        }
+    }
+}
